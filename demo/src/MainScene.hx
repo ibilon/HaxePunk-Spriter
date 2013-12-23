530e957107086ecfebbf5e8594a4bb6bfd742308
Valentin Lemiere <me@ibilon.com>
2013-12-23 20:27:43 +0100

    Demo fix

--- demo/src/MainScene.hx
+++ demo/src/MainScene.hx
@@ -1,9 +1,12 @@
 import com.haxepunk.Scene;
 
+import com.haxepunk.spriter.SpriterEntity;
+
 class MainScene extends Scene
 {
 	public override function begin()
 	{
-
+		var spriter : SpriterEntity = new SpriterEntity("sprites/Test.scml", 100, 300);
+		add(spriter);
 	}
 }
